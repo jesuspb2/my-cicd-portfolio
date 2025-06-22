d4a5d4b1d82d1a97cd723fbe34e3b5a6835b09a1
Jesús Prian <64372399+jesuspb2@users.noreply.github.com>
2025-06-22 19:36:55 +0200

    Update create_terraform_backend.sh

--- cicd/scripts/create_terraform_backend.sh
+++ cicd/scripts/create_terraform_backend.sh
@@ -3,35 +3,35 @@ set -e
 export PAGER=
 
 # ----- Create S3 backend for Terraform -----
-echo "[INFO] Creating S3 bucket: ${BACKEND_BUCKET}"
-if aws s3api head-bucket --bucket "${BACKEND_BUCKET}" 2>/dev/null; then
-  echo "[INFO] Bucket already exists: ${BACKEND_BUCKET}"
+echo "[INFO] Creating S3 bucket: ${backend_bucket}"
+if aws s3api head-bucket --bucket "${backend_bucket}" 2>/dev/null; then
+  echo "[INFO] Bucket already exists: ${backend_bucket}"
 else
   aws s3api create-bucket \
-    --bucket "${BACKEND_BUCKET}" \
-    --region "${AWS_REGION}" \
-    --create-bucket-configuration LocationConstraint="${AWS_REGION}"
+    --bucket "${backend_bucket}" \
+    --region "${aws_region}" \
+    --create-bucket-configuration LocationConstraint="${aws_region}"
   fi
-echo "[INFO] Bucket created: ${BACKEND_BUCKET}"
+echo "[INFO] Bucket created: ${backend_bucket}"
 
 # ----- Enabling versioning -----
 echo "[INFO] Enabling versioning on the bucket"
 aws s3api put-bucket-versioning \
-  --bucket "${BACKEND_BUCKET}" \
+  --bucket "${backend_bucket}" \
   --versioning-configuration Status=Enabled
 
 # ----- Create Dynamo Lock Table -----
-echo "[INFO] Creating DynamoDB table: ${BACKEND_DYNAMODB_TABLE}"
-if aws dynamodb describe-table --table-name "${BACKEND_DYNAMODB_TABLE}" --region "${AWS_REGION}" 2>/dev/null; then
-  echo "[INFO] DynamoDB table already exists: ${BACKEND_DYNAMODB_TABLE}"
+echo "[INFO] Creating DynamoDB table: ${backend_dynamodb_table}"
+if aws dynamodb describe-table --table-name "${backend_dynamodb_table}" --region "${aws_region}" 2>/dev/null; then
+  echo "[INFO] DynamoDB table already exists: ${backend_dynamodb_table}"
 else
   aws dynamodb create-table \
-    --table-name "${BACKEND_DYNAMODB_TABLE}" \
+    --table-name "${backend_dynamodb_table}" \
     --attribute-definitions AttributeName=LockID,AttributeType=S \
     --key-schema AttributeName=LockID,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
-    --region "${AWS_REGION}"
-  echo "[INFO] DynamoDB table created: ${BACKEND_DYNAMODB_TABLE}"
+    --region "${aws_region}"
+  echo "[INFO] DynamoDB table created: ${backend_dynamodb_table}"
 fi
 
-echo "[INFO] Backend ready with bucket: ${BACKEND_BUCKET} and table: ${BACKEND_DYNAMODB_TABLE}"
+echo "[INFO] Backend ready with bucket: ${backend_bucket} and table: ${backend_dynamodb_table}"
