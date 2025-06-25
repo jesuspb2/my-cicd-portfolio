5155f0f29ad293dd6ce60c045aea84c481c2cb38
Jesús Prian Baena <jesuspb2@Jesuss-MacBook-Air.local>
2025-06-26 00:05:47 +0200

    Add cloudfront destroy command

--- cicd/scripts/destroy.sh
+++ cicd/scripts/destroy.sh
@@ -2,7 +2,6 @@
 set -e
 export PAGER=
 
-# ----- Destruir API Gateway -----
 echo "[INFO] Moving into Terragrunt api_gw module directory"
 cd ./cicd/deployment/api_gw || exit
 
@@ -15,6 +14,12 @@ cd ../lambda || exit
 echo "[INFO] Destroying resources for module: lambda"
 terragrunt run-all destroy --terragrunt-non-interactive -no-color
 echo "[INFO] lambda module destroyed successfully"
+
+cd ../cloudfront || exit
+
+echo "[INFO] Destroying resources for module: CloudFront"
+terragrunt run-all destroy --terragrunt-non-interactive -no-color
+echo "[INFO] CloudFront module destroyed successfully"
 
 cd ../s3 || exit
 
