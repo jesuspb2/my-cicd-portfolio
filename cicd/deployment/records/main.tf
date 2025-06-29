87a303a5c9a79ad0f665172429652d54eda599f4
Jesus Prian <jprian@aligntech.com>
2025-06-30 00:46:46 +0200

    Add aws_route53_record data existing

--- cicd/deployment/records/main.tf
+++ cicd/deployment/records/main.tf
@@ -1,8 +1,33 @@
-resource "aws_route53_record" "apex_a" {
+data "aws_route53_record" "apex_a_existing" {
   zone_id = var.zone_id
   name    = var.domain_name
   type    = "A"
+}
 
+data "aws_route53_record" "apex_aaaa_existing" {
+  zone_id = var.zone_id
+  name    = var.domain_name
+  type    = "AAAA"
+}
+
+data "aws_route53_record" "www_a_existing" {
+  zone_id = var.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+}
+
+data "aws_route53_record" "www_aaaa_existing" {
+  zone_id = var.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "AAAA"
+}
+
+resource "aws_route53_record" "apex_a" {
+  count = data.aws_route53_record.apex_a_existing.fqdn != "" ? 0 : 1
+
+  zone_id = var.zone_id
+  name    = var.domain_name
+  type    = "A"
   alias {
     name                   = var.cloudfront_domain_name
     zone_id                = var.cloudfront_zone_id
@@ -11,6 +36,8 @@ resource "aws_route53_record" "apex_a" {
 }
 
 resource "aws_route53_record" "apex_aaaa" {
+  count = data.aws_route53_record.apex_aaaa_existing.fqdn != "" ? 0 : 1
+
   zone_id = var.zone_id
   name    = var.domain_name
   type    = "AAAA"
@@ -23,6 +50,8 @@ resource "aws_route53_record" "apex_aaaa" {
 }
 
 resource "aws_route53_record" "www_a" {
+  count = data.aws_route53_record.www_a_existing.fqdn != "" ? 0 : 1
+
   zone_id = var.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
@@ -35,6 +64,8 @@ resource "aws_route53_record" "www_a" {
 }
 
 resource "aws_route53_record" "www_aaaa" {
+  count = data.aws_route53_record.www_aaaa_existing.fqdn != "" ? 0 : 1
+
   zone_id = var.zone_id
   name    = "www.${var.domain_name}"
   type    = "AAAA"
